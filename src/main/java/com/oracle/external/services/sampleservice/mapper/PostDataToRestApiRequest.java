c63b9eeddf582cfff2a716fbb372a5d58cb40f71
yassua112 <silitongayosua@gmail.com>
2024-02-14 01:03:38 +0700

    update code for restApi

--- src/main/java/com/oracle/external/services/sampleservice/mapper/PostDataToRestApiRequest.java
+++ src/main/java/com/oracle/external/services/sampleservice/mapper/PostDataToRestApiRequest.java
@@ -2,6 +2,21 @@ package com.oracle.external.services.sampleservice.mapper;
 
 import java.io.Serializable;
 
+import lombok.AllArgsConstructor;
+import lombok.Builder;
+import lombok.Data;
+import lombok.NoArgsConstructor;
+
+
+@Data
+@AllArgsConstructor
+@NoArgsConstructor
+@Builder
 public class PostDataToRestApiRequest implements Serializable{
     
+    private String service_id;
+    private String order_type;
+    private String type;
+    private String trx_id;
+
 }
--- src/main/java/com/oracle/external/services/sampleservice/service/SoapSampleService.java
+++ src/main/java/com/oracle/external/services/sampleservice/service/SoapSampleService.java
@@ -1,9 +1,20 @@
 package com.oracle.external.services.sampleservice.service;
 
+import org.springframework.http.HttpEntity;
+import org.springframework.http.HttpHeaders;
+import org.springframework.http.HttpStatus;
+import org.springframework.http.HttpStatusCode;
+import org.springframework.http.MediaType;
+import org.springframework.http.ResponseEntity;
 import org.springframework.stereotype.Service;
 import org.springframework.util.ObjectUtils;
+import org.springframework.web.client.RestTemplate;
+
 
 import com.oracle.external.services.sampleservice.header.Authenticationheader;
+import com.oracle.external.services.sampleservice.mapper.GetDataFromRestApiResponse;
+import com.oracle.external.services.sampleservice.mapper.PostDataToRestApiRequest;
+import com.oracle.external.services.sampleservice.mapper.RequestRestApi;
 import com.oracle.external.services.sampleservice.mapper.ResponseRestApi;
 import com.oracle.external.services.sampleservice.request.v1.Sampleservicerq;
 import com.oracle.external.services.sampleservice.response.v1.Sampleservicers;
@@ -11,11 +22,15 @@ import com.oracle.external.services.sampleservice.response.v1.Sampleservicers;
 @Service
 public class SoapSampleService {
 
+    private final static String URI_LOCAL_REST = "http://localhost:8080/external/services/rest/sample-service";
+
     public Sampleservicers isTransactionValid(Sampleservicerq resquest, Authenticationheader headerRequest) throws Exception{
         dataValidation(resquest,headerRequest);
-
+        ResponseRestApi dataResponse = getDataFromRestApiService(resquest);
         Sampleservicers response = new Sampleservicers();
-        response.setErrorCode("23123");
+        response.setErrorCode(dataResponse.getSampleservicers().getError_code());
+        response.setErrorMsg(dataResponse.getSampleservicers().getError_msg());
+        response.setTrxId(dataResponse.getSampleservicers().getTrx_id());
         return response;
     }
 
@@ -40,9 +55,33 @@ public class SoapSampleService {
 
     }
 
+    @SuppressWarnings("null")
     private ResponseRestApi getDataFromRestApiService(Sampleservicerq resquest){
+        GetDataFromRestApiResponse responseData = new GetDataFromRestApiResponse();
+        try{
+        HttpHeaders headers = new HttpHeaders();
+        headers.setContentType(MediaType.APPLICATION_JSON);
+        RestTemplate restTemplate = new RestTemplate();
+        HttpEntity<Object> requestEntitiy = new HttpEntity<>(RequestRestApi.builder().sampleservicerq(PostDataToRestApiRequest.builder()
+                                                                                                        .service_id(resquest.getServiceId())
+                                                                                                        .type(resquest.getType())
+                                                                                                        .order_type(resquest.getOrderType())
+                                                                                                        .trx_id(resquest.getTrxId())
+                                                                                                        .build())
+                                                                                                        .build());
 
-
-        return null;
+        ResponseEntity<ResponseRestApi> response = restTemplate.postForEntity(URI_LOCAL_REST, requestEntitiy, ResponseRestApi.class);
+        if(response.getBody().getSampleservicers().getError_code().equals("200") ){
+            throw new Exception(response.getBody().getSampleservicers().getError_msg());
+        }
+        responseData.setError_code(response.getBody().getSampleservicers().getError_code());
+        responseData.setError_msg(response.getBody().getSampleservicers().getError_msg());
+        responseData.setTrx_id(response.getBody().getSampleservicers().getTrx_id());
+        }catch(Exception e){
+            e.getLocalizedMessage();
+        }
+        
+        return ResponseRestApi.builder().sampleservicers(responseData).build(); 
+    
     }
 }
